a0ea6362a964d96c93e92f82283663e33f93c0f6
McTyler Tong <mctylert245@gmail.com>
2024-10-16 12:09:43 -0700

    Initial Commit

---
+++ BetterEDU Resources/BetterEDU_ResourcesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterEDU_ResourcesApp.swift
+//  BetterEDU Resources
+//
+//  Created by McTyler Tong on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterEDU_ResourcesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
