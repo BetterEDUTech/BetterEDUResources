b520767db910fba3b6c1bdbd05701720fce994f3
mtong10 <mtong10@asu.edu>
2024-10-16 17:32:23 -0700

    Testing Push/Pull from XCode

--- BetterEDU Resources/ContentView.swift
+++ BetterEDU Resources/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("BetterEDU Resources App Testing Push/Pull")
         }
         .padding()
     }
