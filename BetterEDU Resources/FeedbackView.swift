6a76ad4ed7655480daf582b58aef0e4c75c2330e
narana2 <narana2@asu.edu>
2024-10-29 13:55:26 -0700

    Created and updated code for Feedback Page

---
+++ BetterEDU Resources/FeedbackView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedbackView.swift
+//  BetterEDU Resources
+//
+//  Created by Nick Arana on 10/29/24.
+//
+
+import SwiftUI
+
+struct FeedbackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedbackView()
+}
